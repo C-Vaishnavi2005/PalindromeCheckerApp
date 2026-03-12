1543a5f5498c798a296ce6a372e215697f83db8f
C-Vaishnavi2005 <vaishnavi.csekar@gmail.com>
2026-03-12 10:17:25 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,23 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to PalindromeChecker App");
-        System.out.println("Version: 1.0.0");
+        String testString = "racecar";
+        if (isPalindrome(testString)) {
+            System.out.println(testString + " is a palindrome");
+        } else {
+            System.out.println(testString + " is not a palindrome");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
